cbfb06c4601c416e31b200f8c78ec3e75dfc17d9
Maksym <maximuschampion2002@gmail.com>
2025-05-03 15:35:06 +0300

    add added fields to the model, also serializers, written router for urls.py

---
+++ musician/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from musician.models import Musician
+
+
+class MusicianSerializer(serializers.ModelSerializer):
+    is_adult = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Musician
+        fields = "__all__"
+
+    def validate_age(self, value):
+        if value < 14:
+            raise serializers.ValidationError("Musicians must be at least 14 years old.")
+        return value
--- musician/urls.py
+++ musician/urls.py
@@ -1,3 +1,13 @@
-urlpatterns = []
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from musician.view import MusicianViewSet
+
+router = DefaultRouter()
+router.register("manage", MusicianViewSet, basename="manage")
 
 app_name = "musician"
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
